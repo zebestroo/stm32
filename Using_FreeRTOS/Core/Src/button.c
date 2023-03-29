c493d6f813d9cd154d7c1f6c9216b29126f8d3ac
zebestroo <zebestroo@gmail.com>
2023-03-29 10:40:51 +0300

    Button rattles fixed

--- Using_FreeRTOS/Core/Src/button.c
+++ Using_FreeRTOS/Core/Src/button.c
@@ -10,19 +10,35 @@ void LisBtn(void * parameter)
   /* USER CODE BEGIN LisBtn */
   /* Infinite loop */
 	SemaphoreHandle_t * sem = parameter;
+
 	for(;;)
 	{
-	  if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0)){
-		  if(uxSemaphoreGetCount(*sem)){
-			  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_9);
-			  xSemaphoreTake(*sem, 1);
-		  }
-		  else{
-			  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_9);
-			  xSemaphoreGive(*sem);
-		  }
+		if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0)){
+			uint8_t button_click_accepted = 1;
+			uint8_t counter = 5;
+			while(counter){
+				if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0)){
+					osDelay(20);
+				}
+				else{
+					button_click_accepted = 0;
+					break;
+				}
+				counter--;
+			}
+			if(button_click_accepted){
+				if(uxSemaphoreGetCount(*sem)){
+					HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_9);
+					xSemaphoreTake(*sem, 1);
+				}
+				else{
+					HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_9);
+					xSemaphoreGive(*sem);
+				}
+			}
+
 	  }
-	  osDelay(1);
+	  //osDelay(150);
 	}
   /* USER CODE END LisBtn */
 }
